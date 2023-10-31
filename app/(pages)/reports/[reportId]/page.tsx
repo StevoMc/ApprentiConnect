e93d85d94c1b754ab4ffe522bcdbe12c9fec9593
StevoMc <stevenmc59@gmail.com>
2023-10-31 09:30:24 +0000

    removed type

--- app/(pages)/reports/[reportId]/page.tsx
+++ app/(pages)/reports/[reportId]/page.tsx
@@ -2,19 +2,7 @@ import { useParams, usePathname } from "next/navigation";
 import ReportCard from "../report-card";
 import prisma from "@/lib/prisma";
 
-type ReportCardProps = {
-  id: number;
-  title: string;
-  content: string | null;
-  published: boolean;
-  authorId: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-  date: Date;
-  params: { reportId: string };
-};
-
-const ReportPage = async ({ params, ...report }: Partial<ReportCardProps>) => {
+const ReportPage = async ({ params }: { params: { reportId: string } }) => {
   if (!params?.reportId) return null;
   const rep = await prisma?.report?.findUnique({
     where: { id: Number(params?.reportId) },
