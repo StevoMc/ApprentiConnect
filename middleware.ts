9e1cfb4ca7d52657ebbe536f98773013516d6f79
StevoMc <stevenmc59@gmail.com>
2024-04-21 19:30:37 +0200

    Refactor middleware function in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,24 +5,24 @@ export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/dashboard", "/reports"] };
 
-// export async function middleware(req: NextRequest) {
-//   // Get the pathname of the request (e.g. /, /protected)
-//   const path = req.nextUrl.pathname;
+export async function middleware(req: NextRequest) {
+  // Get the pathname of the request (e.g. /, /protected)
+  const path = req.nextUrl.pathname;
 
-//   // If it's the root path, just render it
-//   if (path === "/") {
-//     return NextResponse.next();
-//   }
+  // If it's the root path, just render it
+  if (path === "/") {
+    return NextResponse.next();
+  }
 
-//   const session = await getToken({
-//     req,
-//     secret: process.env.NEXTAUTH_SECRET,
-//   });
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
-//   if (!session && path === "/dashboard") {
-//     return NextResponse.redirect(new URL("/signin", req.url));
-//   } else if (session && (path === "/login" || path === "/signup")) {
-//     return NextResponse.redirect(new URL("/protected", req.url));
-//   }
-//   return NextResponse.next();
-// }
+  if (!session && path === "/dashboard") {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  } else if (session && (path === "/login" || path === "/signup")) {
+    return NextResponse.redirect(new URL("/protected", req.url));
+  }
+  return NextResponse.next();
+}
